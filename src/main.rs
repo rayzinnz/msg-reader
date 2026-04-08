d035f9283dc1b7d13c112dcacf836249d740be43
Ray <raymond.gabriel@gmail.com>
2026-04-08 15:36:20 +1200

    test msg_in_msg

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::path::Path;
 use msg_reader::{get_msg_from_file};
 
 fn main() {
-    let filepath = Path::new(r"C:\Users\hrag\OutlookData\ITSolutionsTickets\2026\00000000812638D851360E448831BA5A9840D7BF07002E78EC35ADE8584380BC921B7514433600000000770300002E78EC35ADE8584380BC921B7514433600015D8912CE0000.msg");
+    let filepath = Path::new(r"C:\Users\hrag\temp\msg_examples\msg_in_msg.msg");
     let msg = get_msg_from_file(filepath).unwrap();
 
     println!("{:#?}", msg.recipients);
+    println!("{:#?}", msg.sub_msgs[0].recipients);
+    println!("{:#?}", msg.sub_msgs[0].attachments[0].display_name);
 }
